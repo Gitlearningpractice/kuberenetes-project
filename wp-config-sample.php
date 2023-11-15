b96e1f1832aa5304c62482b6eb0873dadcb70286
Zalak <shahzalak561@gmail.com>
2023-11-15 06:38:14 +0000

    kubernetes project

---
+++ wp-config-sample.php
@@ -0,0 +1,9 @@
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'database1' );
+/** MySQL database username */
+define( 'DB_USER', 'sandry' );
+/** MySQL database password */
+define( 'DB_PASSWORD', 'password' );
+/** MySQL hostname */
+define( 'DB_HOST', 'mysql-service' );
